11ec101c74dcbd3de6e9c9413abc722d8777bca8
Nalin Joshi <120622015+NalinJ0shi@users.noreply.github.com>
2023-01-15 16:23:23 +0530

    dino commits

---
+++ script.js
@@ -0,0 +1,168 @@
+
+//board
+let board;
+let boardWidth = 750;
+let boardHeight = 250;
+let context;
+//music
+const gameoversound = new Audio('gameover.mp3');
+const movesound = new Audio('move.mp3');
+
+//dino
+let dinoWidth = 88;
+let dinoHeight = 94;
+let dinoX = 50;
+let dinoY = boardHeight - dinoHeight;
+let dinoImg;
+
+let dino = {
+    x : dinoX,
+    y : dinoY,
+    width : dinoWidth,
+    height : dinoHeight
+}
+
+//cactus
+let cactusArray = [];
+
+let cactus1Width = 34;
+let cactus2Width = 69;
+let cactus3Width = 102;
+
+let cactusHeight = 70;
+let cactusX = 700;
+let cactusY = boardHeight - cactusHeight;
+
+let cactus1Img;
+let cactus2Img;
+let cactus3Img;
+
+//physics
+let velocityX = -8; //cactus moving left speed
+let velocityY = 0;
+let gravity = .4;
+
+let gameOver = false;
+let score = 0;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+
+    context = board.getContext("2d"); //used for drawing on the board
+    dinoImg = new Image();
+    dinoImg.src = "dino.png";
+    dinoImg.onload = function() {
+        context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+    }
+
+    cactus1Img = new Image();
+    cactus1Img.src = "cactus1.png";
+
+    cactus2Img = new Image();
+    cactus2Img.src = "cactus2.png";
+
+    cactus3Img = new Image();
+    cactus3Img.src = "cactus3.png";
+
+    requestAnimationFrame(update);
+    setInterval(placeCactus, 1000); //1000 milliseconds = 1 second
+    document.addEventListener("keydown", moveDino);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    if (gameOver) {
+        return;   
+    }
+    context.clearRect(0, 0, board.width, board.height);
+
+    //dino
+    velocityY += gravity;
+    dino.y = Math.min(dino.y + velocityY, dinoY); //apply gravity to current dino.y, making sure it doesn't exceed the ground
+    context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+
+    //cactus
+    for (let i = 0; i < cactusArray.length; i++) {
+        let cactus = cactusArray[i];
+        cactus.x += velocityX;
+        context.drawImage(cactus.img, cactus.x, cactus.y, cactus.width, cactus.height);
+
+        if (detectCollision(dino, cactus)) {
+            gameOver = true;
+            dinoImg.src = "dino-dead.png";
+            gameoversound.play();
+            alert('game over');
+            dinoImg.onload = function() {
+                context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+            }
+        }
+    }
+
+    //score
+    context.fillStyle="black";
+    context.font="20px courier";
+    score++;
+    context.fillText(score, 5, 20);
+}
+
+function moveDino(e) {
+    if (gameOver) {
+        return;
+    }
+
+    if ((e.code == "Space" || e.code == "ArrowUp") && dino.y == dinoY) {
+        movesound.play();
+
+        //jump
+        velocityY = -10;
+    }
+}
+
+function up(){
+    movesound.play();
+
+    //jump
+    velocityY = -10;
+}
+
+function placeCactus() {
+    if (gameOver) {
+        return;
+    }
+
+    //place cactus
+    let cactus = {
+        img : null,
+        x : cactusX,
+        y : cactusY,
+        width : null,
+        height: cactusHeight
+    }
+
+    let placeCactusChance = Math.random(); 
+
+    if (placeCactusChance > .90) { //10% you get cactus3
+        cactus.img = cactus3Img;
+        cactus.width = cactus3Width;
+        cactusArray.push(cactus);
+    }
+    else if (placeCactusChance > .60) { //40% you get cactus2
+        cactus.img = cactus2Img;
+        cactus.width = cactus2Width;
+        cactusArray.push(cactus);
+    }
+    else if (placeCactusChance > .40) { //60% you get cactus1
+        cactus.img = cactus1Img;
+        cactus.width = cactus1Width;
+        cactusArray.push(cactus);
+    }
+}
+
+function detectCollision(obj1, obj2) {
+    return obj1.x < obj2.x + obj2.width &&   
+           obj1.x + obj1.width > obj2.x &&
+           obj1.y < obj2.y + obj2.height &&  
+           obj1.y + obj1.height > obj2.y;   
+}
